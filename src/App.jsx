8095d29b0b7714aec34657769bb1ac1b167b18d2
李倩如 <876794103@qq.com>
2024-06-04 13:34:53 +0800

    feat: line-config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './index.js';
 import D3Line from './components/d3-line.jsx';
 const options = {
+  width: 400,
+  height: 200,
+  marginLeft: 30,
+  marginRight: 30,
+  marginBottom: 20,
   xAxis: {
     data: ['周一', '周二', '周三', '周四', '周五'],
   },
--- src/components/d3-line.jsx
+++ src/components/d3-line.jsx
@@ -10,15 +10,26 @@ const generateXTickGap = ({ length, marginLeft, marginRight, width }) => {
 };
 
 export default function D3AxisLine({ options }) {
+  const {
+    marginBottom = 0,
+    marginTop = 0,
+    marginLeft = 0,
+    marginRight = 0,
+    width = 0,
+    height = 0,
+  } = options;
   useEffect(() => {
     const xTicks = generateXTickGap({
-      width: 400,
-      marginLeft: 30,
-      marginRight: 30,
+      width: width,
+      marginLeft: marginLeft,
+      marginRight: marginRight,
       length: options.xAxis.data.length,
     });
     const x = d3.scaleOrdinal(options.xAxis.data, xTicks);
-    const y = d3.scaleLinear([0, d3.max(options.yAxis.data)], [175, 10]);
+    const y = d3.scaleLinear(
+      [0, d3.max(options.yAxis.data)],
+      [height - marginTop - marginBottom, marginBottom]
+    );
 
     const svg = d3
       .select('.main')
@@ -29,13 +40,20 @@ export default function D3AxisLine({ options }) {
 
     svg
       .append('g')
-      .attr('transform', `translate(0, 180)`)
-      .call(d3.axisBottom(x));
+      .attr('transform', `translate(0, ${height - marginBottom})`)
+      .call(d3.axisBottom(x).tickPadding(10).tickSizeInner(0).tickSizeOuter(0));
 
     svg
       .append('g')
-      .attr('transform', `translate(30, 0)`)
-      .call(d3.axisLeft(y).ticks(4));
+      .attr('transform', `translate(${marginLeft}, 0)`)
+      .call(
+        d3
+          .axisLeft(y)
+          .tickPadding(10)
+          .ticks(4)
+          .tickSizeInner(0)
+          .tickSizeOuter(0)
+      );
 
     const line = d3
       .line()
@@ -52,6 +70,9 @@ export default function D3AxisLine({ options }) {
   }, [options.xAxis]);
 
   return (
-    <div style={{ width: '400px', height: '200px' }} className="main"></div>
+    <div
+      style={{ width: width + 'px', height: height + 'px' }}
+      className="main"
+    ></div>
   );
 }
