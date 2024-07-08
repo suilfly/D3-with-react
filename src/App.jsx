8c41c8df8d8c54fb8dde50ee34518329cb42eeef
李倩如 <876794103@qq.com>
2024-07-08 18:05:45 +0800

    feat: react-router-学习

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,21 @@
 import './index.js';
-import D3Line from './components/d3-line.jsx';
-const options = {
-  width: 400,
-  height: 200,
-  marginLeft: 30,
-  marginRight: 30,
-  marginBottom: 20,
-  xAxis: {
-    data: ['周一', '周二', '周三', '周四', '周五'],
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './router/root.jsx';
+import LeftContainer from './router/leftContainer.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: 'nav/:id',
+        element: <LeftContainer />,
+      },
+    ],
   },
-  yAxis: {
-    data: [12, 34, 100, 22, 32],
-  },
-  data: [],
-};
-
-options.data = options.xAxis.data.map((x, i) => ({
-  day: x,
-  value: options.yAxis.data[i],
-}));
-
-async function testProxy() {
-  const response = await fetch('/api', {
-    method: 'get',
-  });
-  console.log(response);
-}
-
-testProxy();
+]);
 
 export default function App() {
-  return (
-    <div>
-      <D3Line options={options} />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
---
+++ src/router/leftContainer.jsx
@@ -0,0 +1,3 @@
+export default function LeftContainer() {
+  return <div className="left-container-wrapper"></div>;
+}
