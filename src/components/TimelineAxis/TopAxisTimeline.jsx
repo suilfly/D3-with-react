4dcdc2baa3251ea44c41e094d7541399926e38e2
李倩如 <876794103@qq.com>
2024-07-18 14:06:43 +0800

    feat: 时间轴开发-3

---
+++ src/components/TimelineAxis/TopAxisTimeline.jsx
@@ -0,0 +1,43 @@
+import markerPointIcon from '@/assets/imgs/marker-pointer.png';
+
+export default function TopAxisTimeline({ axisWidth }) {
+  function renderTopAxisLabels() {
+    const labelCount = 21;
+    const labelSecondsInterval =
+      (timeInfo.currentRange * 60) / (labelCount - 1);
+    const labelWidth = 1193 / (labelCount - 1);
+
+    return new Array(labelCount).fill().map((_, index) => {
+      let timestamp =
+        timeInfo.axisPointStartTimeStamp + index * labelSecondsInterval * 1000;
+
+      if (index === 0) {
+        timestamp = timeInfo.axisPointStartTimeStamp;
+      }
+      const label = getDateStringByTimestamp(timestamp, 'nodate');
+      return (
+        <div
+          className="timeline-axis-label"
+          style={{ width: `${labelWidth}px` }}
+          key={index}
+        >
+          <p>{label}</p>
+        </div>
+      );
+    });
+  }
+
+  return (
+    <div className="timeline-axis-time">
+      <div
+        className="timeline-inner-progress"
+        style={{
+          width: axisWidth + 'px',
+        }}
+      >
+        <img className="timeline-marker" src={markerPointIcon} />
+      </div>
+      <div className="timeline-axis-label-wrapper">{renderTopAxisLabels()}</div>
+    </div>
+  );
+}
